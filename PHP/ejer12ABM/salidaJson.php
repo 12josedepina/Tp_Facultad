35526f77a3ab653b8e8cb751ee64775b544bc43d
joseq <quispevladimir5@gmail.com>
2021-06-23 23:38:04 -0300

    commmit

---
+++ PHP/ejer12ABM/salidaJson.php
@@ -0,0 +1,60 @@
+<?php
+
+sleep(2);
+
+define("USER","uweixrhkpumlwxik");
+define("PASSWORD","mpZ3cUp30vUFrvGMcSfs");
+define("HOST","bwmfm2ptyu7kv8krlfru-mysql.services.clever-cloud.com");
+define("DB_NAME","bwmfm2ptyu7kv8krlfru");
+
+$fp = fopen('logs_articulos.txt', 'w');
+
+$mysqli = new mysqli(HOST,USER , PASSWORD, DB_NAME);
+
+
+if ($mysqli->connect_errno) {
+    
+    fwrite($fp, "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error);
+
+}else{
+    
+    fwrite($fp,"Conexión a base de datos exitosa!!");
+
+    
+}
+
+
+$query = "SELECT * FROM articulos";
+
+$articulosObj = [];
+
+if ($resultado = $mysqli->query($query)) {
+
+    /* obtener un array asociativo */
+    /*$fila contiene cada registro en cada iteracion*/
+    
+    while ($fila = $resultado->fetch_assoc()) {
+    
+        $articulo  = new stdClass;
+        $articulo->CodArt = $fila["codigo"];
+        $articulo->Familia = $fila["familia"];
+        $articulo->Um = $fila["unidad_medida"];
+        $articulo->Descripcion = $fila["descripcion"];
+        $articulo->FechaAlta = $fila["fecha_alta"];
+        $articulo->SaldoStock = $fila["stock"];        
+    
+        array_push($articulosObj,$articulo);
+            
+    }
+
+    /* liberar el conjunto de resultados */
+    $resultado->free();
+}
+
+echo json_encode($articulosObj);
+
+/* cerrar la conexión */
+$mysqli->close();
+
+fclose($fp);
+?>
