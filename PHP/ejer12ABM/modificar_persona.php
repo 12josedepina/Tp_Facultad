f6febb9c96bd6e1383f138fc4b23c98e83050996
Miguel <miguel.qma@gmail.com>
2021-06-30 01:33:02 -0300

    MODIFICAR PERSONA TERMINADO

---
+++ PHP/ejer12ABM/modificar_persona.php
@@ -0,0 +1,54 @@
+<?php
+
+include("conexion_db.php");
+
+
+$fp = fopen('actualizar_personas.txt', 'w');
+
+
+$nombre = $_POST["nombre"];
+$apellido   = $_POST["apellido"];
+$numero_documento = $_POST["numero_documento"];
+$tipo_documento = $_POST["tipo_documento"];
+$email = $_POST["email"];
+$fecha_nacimiento = $_POST["fecha_nacimiento"];
+
+
+$queryIdTipoDocumento = " SELECT id FROM tipo_documento where nombre = ".$tipo_documento;
+
+$id_tipo_documento = 1;//seteado por default DNI
+
+if ($resultado = $mysqli->query($queryIdTipoDocumento)) {
+    
+    $fila = $resultado->fetch_assoc();
+
+    $id_tipo_documento = $fila["id"];
+
+}
+
+$query = " UPDATE personas set "; 
+$query.= " nombre='".$nombre."' ";
+$query.= ",apellido='".$apellido."' ";
+$query.= ",fecha_de_nac='".$fecha_nacimiento."' ";
+$query.= ",email='".$email."' ";
+$query.= ",id_tipo_documento=".$id_tipo_documento;
+$query.= " where numero_documento = ".$numero_documento;
+
+
+$persona  = new stdClass;
+
+if (!$mysqli->query($query)) {
+
+    echo "ERROR AL INTENTAR ACTUALIZAR LOS DATOS DE LA PERSONA".$mysqli->error;
+}
+
+echo "Se actualizo correctamente los datos";
+echo "nombre : ".$nombre;
+echo "documento : ".$numero_documento;
+
+/* cerrar la conexión */
+$mysqli->close();
+
+fclose($fp);
+
+?>
---
+++ PHP/ejer12ABM/obtener_documentos.php
@@ -0,0 +1,41 @@
+<?php
+
+include("conexion_db.php");
+
+
+$fp = fopen('obtener_documentos.txt', 'w');
+
+
+$query = " SELECT * FROM tipo_documento ";
+
+$documentos  = [];
+
+if ($resultado = $mysqli->query($query)) {
+
+    /* obtener un array asociativo */
+    /*$fila contiene cada registro en cada iteracion*/
+    
+    while($fila = $resultado->fetch_assoc()){
+
+
+        $documento = new stdClass;
+
+        $documento->nombre = $fila["nombre"];
+
+        $documento->descripcion = $fila["descripcion"];
+
+        array_push($documentos,$documento);
+    }
+    
+    /* liberar el conjunto de resultados */
+    $resultado->free();
+}
+
+echo json_encode($documentos);
+
+/* cerrar la conexión */
+$mysqli->close();
+
+fclose($fp);
+
+?>
