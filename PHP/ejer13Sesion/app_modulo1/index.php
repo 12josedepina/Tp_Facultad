05ac893cafb31699441b9eb112f4ba7786109f59
joseq <quispevladimir5@gmail.com>
2021-07-02 06:27:19 -0300

    modificacion ahora

--- PHP/ejer13Sesion/app_modulo1/index.php
+++ PHP/ejer13Sesion/app_modulo1/index.php
@@ -9,7 +9,7 @@ include("../valida_sesion.php");
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lista</title>
+    <title>DB-ABM</title>
     <link rel="stylesheet" href="./estilo.css">
 </head>
 
@@ -349,6 +349,7 @@ include("../valida_sesion.php");
                     $("#ventanaModalRespuestaModificar").show();
                     
                     $("#respuesta-modificar-persona").html(respuesta);
+                    cargaTabla();
 
                 },
                 error: function(){
